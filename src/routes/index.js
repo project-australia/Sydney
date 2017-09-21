89ec41f0f55f3c7191154403aad5149d2b15eba9
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-21 09:46:09 -0300

    style: add standardJS linter and lint code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,27 @@
-var home = require('./home');
-var health = require('./health');
+var home = require('./home')
+var health = require('./health')
 
 function configureRoutes (app) {
-  app.use('/', home);
-  app.use('/health', health);
+  app.use('/', home)
+  app.use('/health', health)
 
   // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+  app.use(function (req, res, next) {
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
+  })
 
   // error handler
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
     // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
+    res.status(err.status || 500)
+    res.render('error')
+  })
 }
 
-module.exports = configureRoutes;
+module.exports = configureRoutes
