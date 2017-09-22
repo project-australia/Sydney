be193d522ad7c5d3e2a93fec8787f35fc5bc078c
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-22 18:19:38 -0300

    test: disabling mongo for test env

--- __tests__/unit/src/controllers/someController.test.js
+++ __tests__/unit/src/controllers/someController.test.js
@@ -6,9 +6,12 @@ jest.mock('../../../../src/services/someService')
 
 describe('Some Controller test', () => {
   it('should find all some objects', async () => {
-    SomeService.findAllSomeObject = jest.fn()
+    const dummySomeObject = { id: 'unique_id' }
+    SomeService.findAllSomeObject = jest.fn(() => Promise.resolve(dummySomeObject))
     const response = await request(app).get('/some/')
+
     expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual(dummySomeObject)
     expect(SomeService.findAllSomeObject).toHaveBeenCalled()
   })
 })
--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,10 @@ import dotenv from 'dotenv'
 import { connectMongo } from './mongoose'
 
 export function initConfigurations () {
+  if (process.env.NODE_ENV === 'test') {
+    return
+  }
+
   dotenv.config()
   connectMongo()
 }
