76834b13f9cfb416bd3746a9198dfc67ca48181d
Eduardo Moroni <eduardomoroni@gmail.com>
2017-11-29 20:45:25 -0200

    build: fix name debug

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
 const dotenv = require('dotenv')
 const { mongooseConfig } = require('./mongoose')
+const { initializeFirebaseAdmin } = require('./firebase')
 
 async function initConfigurations () {
   dotenv.config()
   await mongooseConfig(process.env.NODE_ENV)
+  await initializeFirebaseAdmin()
 }
 
 module.exports = { initConfigurations }
