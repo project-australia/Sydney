bce9f7db63d5fb7badba2e51d60622c7459780d4
Hebert Porto <hebertporto@gmail.com>
2017-12-21 23:16:57 -0200

    Logica para avaliar o o preco do livro

--- src/controllers/pricingController.js
+++ src/controllers/pricingController.js
@@ -1,9 +1,30 @@
 const { getEvaluationPrice } = require('../services/amazonProductApi')
+// const { responseAmazon } = require('./amazonreturn')
 
 const getBookEvaluation = async (req, res) => {
   const { isbn } = req.query
   const result = await getEvaluationPrice(isbn)
-  res.status(200).json({ result })
+  const obj = result.find(obj => {
+    if (obj['ItemAttributes'][0]['EAN']) {
+      return obj['ItemAttributes'][0]['EAN'][0] === isbn
+    }
+    return false
+  })
+  let price = ''
+  let real = ''
+  if (obj['SalesRank'][0]) {
+    const u = obj['OfferSummary'][0]['LowestUsedPrice'][0]['FormattedPrice'][0]
+    const bestOffer = u.substr(1)
+    real = parseFloat(bestOffer)
+    if (parseInt(obj['SalesRank'][0], 10) < 600000) {
+      price = real * 0.20
+    } else if (parseInt(obj['SalesRank'][0], 10) < 900000) {
+      price = real * 0.20
+    } else {
+      price = -1
+    }
+  }
+  res.status(200).json({ obj, price: price.toFixed(2), real })
 }
 
 module.exports = { getBookEvaluation }
@@ -33,3 +54,10 @@ module.exports = { getBookEvaluation }
 // SalesRank
 
 // REQUEST EXAMPLE localhost:3000/pricing?isbn=9781483358505
+
+// If rank 1 - 600, 000 on Amazon buy for 25 % of lowest price on Amazon
+// Have to be modifiable
+// Price will change often since prices change multiple times per day.Also the cheapest book will get sold so the price will reflect the new lowest price
+// If rank 600, 001 - 900, 000 on Amazon buy for 20 % of lowest price on Amazon
+// Have to be modifiable
+// Price will change often since prices change multiple times per day.Also the cheapest book will get sold so the price will reflect the new lowest price
