3dfe3eb26c7445d5564dc195d082198847adc4c7
Eduardo Moroni <eduardomoroni@gmail.com>
2017-12-29 10:52:47 -0200

    feat: remove isbn validation

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,26 +1,4 @@
 const EvaluationService = require('../services/bookEvaluation')
-const { ApiError } = require('./apiError')
-
-const isInvalidISBN = (isbn, res) => {
-  const formattedIsbn = isbn.replace(/-/, '').trim()
-  if (isNaN(formattedIsbn)) {
-    res.status(400).json(new ApiError({message: 'Invalid ISBN, please check it out.'})).end()
-    return true
-  }
-  return false
-}
-
-const handleControllerErrors = function (error, res) {
-  const apiErrorModel = new ApiError(error)
-  if (error.name === 'ClientError') {
-    res.status(404).json(apiErrorModel) // FIXME: just an example of semantic status code
-  } else if (error.name === 'ServiceError') {
-    res.status(400).json(apiErrorModel) // FIXME: just an example of semantic status code
-  } else {
-    res.status(500).json(apiErrorModel) // FIXME: just an example of semantic status code
-  }
-  res.end()
-}
 
 const evaluate = async (req, res) => {
   const isbn = req.query.isbn || req.params.isbn
@@ -29,50 +7,13 @@ const evaluate = async (req, res) => {
   const formattedIsbn = isbn.replace(/-/, '')
                             .replace(/-/, '')
                             .replace(/-/, '')
-                            .replace(/-/, '').trim()
+                            .replace(/-/, '')
+                            .trim()
 
-  if (isInvalidISBN(formattedIsbn, res)) {
-    return null
-  }
-
-  try {
-    const evaluation = await EvaluationService.evaluateBook(formattedIsbn)
-    res.status(200).json(evaluation)
-  } catch (error) {
-    handleControllerErrors(error, res)
-  }
-}
-
-const details = async (req, res) => {
-
-}
-
-const find = async (req, res) => {
-
-}
-
-const donate = async (req, res) => {
-
-}
-
-const rent = async (req, res) => {
-
-}
-
-const sell = async (req, res) => {
-
-}
-
-const buy = async (req, res) => {
-
+  const evaluation = await EvaluationService.evaluateBook(formattedIsbn)
+  res.status(200).json(evaluation)
 }
 
 module.exports = {
-  buy,
-  find,
-  rent,
-  sell,
-  donate,
-  details,
   evaluate
 }
