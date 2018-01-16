7aaf7cce0e09f82c76f8bed89bd11fe3c4f7eb83
Hebert Porto <hebertporto@gmail.com>
2018-01-16 21:30:03 -0200

    added fields em order model, update tests

--- src/mongoose/models/orderModel.js
+++ src/mongoose/models/orderModel.js
@@ -5,15 +5,25 @@ const { parserOptions } = require('../../config/mongoose')
 
 const Schema = mongoose.Schema
 
+const address = {
+  city: {type: String, lowercase: true, trim: true},
+  street: {type: String, lowercase: true, trim: true},
+  number: {type: String, lowercase: true, trim: true},
+  zipCode: {type: String, lowercase: true, trim: true},
+  state: {type: String, lowercase: true, trim: true}
+}
+
 let OrderSchema = new Schema({
   customerId: {type: String, ref: userCollection},
   type: {type: String, enum: ['RENT', 'BUY', 'SELL']},
+  trackingPaymentId: {type: String, trim: true},
   status: {
     type: String,
     enum: ['WAITING_PAYMENT', 'PAYMENT_CONFIRMED', 'CANCELLED', 'RECEIVED', 'SHIPPED'],
     default: 'WAITING_PAYMENT'
   },
-  items: [{type: Schema.ObjectId, ref: bookCollection}]
+  items: [{type: Schema.ObjectId, ref: bookCollection}],
+  shippingAdress: address
 })
 
 OrderSchema.set('toJSON', parserOptions)
