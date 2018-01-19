48b5eab8e1fb249a30d23bff6930d46594c8d653
Hebert Porto <hebertporto@gmail.com>
2018-01-18 22:27:28 -0200

    adicionando rota para pesquisar por um livro e adcionar livros

--- src/router/books.js
+++ src/router/books.js
@@ -1,12 +1,16 @@
 const { Router } = require('express')
 const {
   evaluate,
-  lookup
+  lookup,
+  findBookByParams,
+  addNewBooks
 } = require('../controllers/bookController')
 
 const router = Router()
 
 router.post('/:isbn/evaluation', evaluate)
 router.post('/:isbn/lookup', lookup)
+router.post('/search', findBookByParams)
+router.post('/', addNewBooks)
 
 module.exports = router
