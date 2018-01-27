fc24f50298bb3eca2cf03cf8ffa5aa915aabba54
Eduardo Moroni <eduardomoroni@gmail.com>
2018-01-27 12:39:28 -0200

    refactor: remove redundant book from /books/ routes

--- src/router/books.js
+++ src/router/books.js
@@ -14,7 +14,7 @@ router.post('/:isbn/evaluation', evaluate)
 router.post('/:isbn/lookup', lookup)
 router.post('/search', findBookByParams)
 router.post('/', addNewBooks)
-router.get('/featuredBooks', getFeaturedBooks)
-router.get('/recentlyAddedBooks', getRecentlyAddedBooks)
+router.get('/featured', getFeaturedBooks)
+router.get('/recentlyAdded', getRecentlyAddedBooks)
 
 module.exports = router
