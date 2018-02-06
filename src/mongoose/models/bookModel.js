5143f5002c3bc08fbfad9918d045106e5c130907
Eduardo Moroni <eduardomoroni@gmail.com>
2018-02-06 20:40:49 -0200

    refactor: change Book schema to receive book condition

--- src/mongoose/models/bookModel.js
+++ src/mongoose/models/bookModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const { parserOptions } = require('../../config/mongoose')
+const {parserOptions} = require('../../config/mongoose')
 
 const Schema = mongoose.Schema
 
@@ -21,14 +21,18 @@ let BookSchema = new Schema({
   sellingPrice: {type: Number},
   buyingPrice: {type: Number},
   featured: {type: Boolean, default: false},
-  title: {type: String, trim: true, lowercase: true},
+  title: {type: String, trim: true, lowercase: true, required: true},
   authors: [{type: String, lowercase: true, trim: true}],
-  isbn: {type: String, trim: true, lowercase: true},
+  isbn: {type: String, trim: true, lowercase: true, required: true},
   edition: {type: String, trim: true, lowercase: true},
   images,
   dimensions,
-  bookCondition: {type: String, enum: ['Used – Acceptable', 'Used – Good', 'Used – Very Good', 'Used – Like New', 'New']}
-}, {timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' }}
+  condition: {
+    type: String,
+    enum: ['Used – Acceptable', 'Used – Good', 'Used – Very Good', 'Used – Like New', 'New'],
+    required: true
+  }
+}, {timestamps: {createdAt: 'createdAt', updatedAt: 'updatedAt'}}
 )
 
 BookSchema.set('toJSON', parserOptions)
