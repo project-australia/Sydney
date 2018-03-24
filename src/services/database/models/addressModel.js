12a4cea1cea9d6eaee33715ac5b2d7018b647df6
Eduardo Moroni <eduardomoroni@gmail.com>
2018-03-24 11:24:54 -0300

    feat: remove required number

--- src/services/database/models/addressModel.js
+++ src/services/database/models/addressModel.js
@@ -1,7 +1,6 @@
 exports.address = {
   city: { type: String, lowercase: true, trim: true, required: true },
   street: { type: String, lowercase: true, trim: true, required: true },
-  number: { type: String, lowercase: true, trim: true, required: true },
   zipCode: { type: String, lowercase: true, trim: true, required: true },
   state: { type: String, lowercase: true, trim: true, required: true }
 }
