1c282c8f00bdb991c605944aaac8c830be240081
Eduardo Moroni <eduardomoroni@gmail.com>
2018-04-07 11:55:14 -0300

    style: remove HTML template top bar

--- src/domain/assets/emailTemplate.js
+++ src/domain/assets/emailTemplate.js
@@ -386,48 +386,6 @@ const header = `
   </tr>
 `
 
-const topBar = `
-<tr>
-    <td align='left' valign='top' width='100%'
-        style='background:repeat-x url(http://s3.amazonaws.com/swu-filepicker/4E687TRe69Ld95IDWyEg_bg_top_02.jpg) #ffffff;'>
-      <center>
-        <img src='http://s3.amazonaws.com/swu-filepicker/SBb2fQPrQ5ezxmqUTgCr_transparent.png'
-             class='force-width-gmail'>
-        <table cellspacing='0' cellpadding='0' width='100%' bgcolor='#ffffff'
-               background='http://s3.amazonaws.com/swu-filepicker/4E687TRe69Ld95IDWyEg_bg_top_02.jpg'
-               style='background-color:transparent'>
-          <tr>
-            <td width='100%' height='80' valign='top' style='text-align: center; vertical-align:middle;'>
-              <center>
-                <table cellpadding='0' cellspacing='0' width='600' class='w320'>
-                  <tr>
-                    <td class='pull-left mobile-header-padding-left' style='vertical-align: middle;'>
-                      <a href=''><img width='137' height='47'
-                                      src='http://s3.amazonaws.com/swu-filepicker/0zxBZVuORSxdc9ZCqotL_logo_03.gif'
-                                      alt='logo'></a>
-                    </td>
-                    <td class='pull-right mobile-header-padding-right' style='color: #4d4d4d;'>
-                      <a href=''><img width='44' height='47'
-                                      src='http://s3.amazonaws.com/swu-filepicker/k8D8A7SLRuetZspHxsJk_social_08.gif'
-                                      alt='twitter'/></a>
-                      <a href=''><img width='38' height='47'
-                                      src='http://s3.amazonaws.com/swu-filepicker/LMPMj7JSRoCWypAvzaN3_social_09.gif'
-                                      alt='facebook'/></a>
-                      <a href=''><img width='40' height='47'
-                                      src='http://s3.amazonaws.com/swu-filepicker/hR33ye5FQXuDDarXCGIW_social_10.gif'
-                                      alt='rss'/></a>
-                    </td>
-                  </tr>
-                </table>
-              </center>
-            </td>
-          </tr>
-        </table>
-      </center>
-    </td>
-  </tr>
-`
-
 const tableHeader = `
 <tr>
   <td class='title-dark' width='300'>
