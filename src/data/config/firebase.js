04c0d3669100b2cd6b444b5c82856fc3baded6b2
Eduardo Moroni <eduardomoroni@gmail.com>
2018-04-23 08:59:51 -0300

    feat: remove firebase admin support

--- src/data/config/firebase.js
+++ src/data/config/firebase.js
@@ -1,18 +1,4 @@
-const admin = require('firebase-admin')
 const { initializeApp } = require('firebase')
-
-const getServiceAccount = () => ({
-  type: 'service_account',
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://accounts.google.com/o/oauth2/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
-})
 
 const firebaseConfig = () => {
   const {
@@ -35,11 +21,7 @@ const firebaseConfig = () => {
 }
 
 async function initializeFirebaseAdmin () {
-  return // TODO: Disabling firebase admin
-  return admin.initializeApp({
-    credential: admin.credential.cert(getServiceAccount()),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
-  })
+  return null
 }
 
 async function initializeFirebase () {
